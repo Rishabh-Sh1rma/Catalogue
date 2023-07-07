90c8fa663e47afb885512081c274049b92a91b6c
Rishabh-Sh1rma <130218510+Rishabh-Sh1rma@users.noreply.github.com>
2023-07-07 09:02:41 +0530

    Create Whatsapp.js

---
+++ src/components/Whatsapp.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Whatsapp.css";
+const Whatsapp = () => {
+  return (
+    <div className="whatsapp_float">
+      <a href="https://wa.me/919664078593" target="_blank">
+        <img
+          src="https://static.vecteezy.com/system/resources/previews/018/930/748/original/whatsapp-logo-whatsapp-icon-whatsapp-transparent-free-png.png"
+          className="whatsapp_float_btn"
+        />
+      </a>
+    </div>
+  );
+};
+
+export default Whatsapp;
